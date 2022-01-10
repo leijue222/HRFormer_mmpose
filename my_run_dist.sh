a909041ea361afb055f2aae0017aade03f9e3ba1
leijue222 <1035897872@qq.com>
2022-01-11 00:50:51 +0800

    update bash

--- my_run_dist.sh
+++ my_run_dist.sh
@@ -3,4 +3,5 @@ df
 CONFIG=$1
 
 # bash tools/my_dist_train.sh configs/$CONFIG.py 8
-bash tools/my_dist_train2.sh configs/$CONFIG.py 8
+# bash tools/my_dist_train2.sh configs/$CONFIG.py 8
+bash tools/my_dist_train3.sh configs/$CONFIG.py 8
